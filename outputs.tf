a92ee7b80031430cbc248eaeca5319754a15ee43
arnol377 <david.j.arnold.jr@census.gov>
2025-01-28 18:24:37 -0500

    Add outputs for AMI, launch configuration, cloud init, and autoscaling group

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "ami" {
+  value = data.aws_ami.ami
+}
+
+output "launch_configuration" {
+  value = aws_launch_configuration.lc
+}
+
+output "cloud_init" {
+  value = data.cloudinit_config.cloud_init
+}
+
+output "autoscaling_group" {
+  value = aws_autoscaling_group.asg
+}

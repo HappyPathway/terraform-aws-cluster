0981bf1952795ae7b29ab9baffdaf4312b8839b3
Dave Arnold <dave@roknsound.com>
2025-01-29 17:45:56 -0800

    Fix autoscaling group reference in lifecycle hook and schedule configurations

--- autoscaling_schedule.tf
+++ autoscaling_schedule.tf
@@ -1,6 +1,6 @@
 resource "aws_autoscaling_schedule" "asg_schedule" {
   for_each               = { for schedule in var.autoscaling_schedule : schedule.scheduled_action_name => schedule }
-  autoscaling_group_name = one(aws_autoscaling_group).asg.id
+  autoscaling_group_name = one(aws_autoscaling_group.asg).id
   scheduled_action_name  = each.value.scheduled_action_name
   start_time             = each.value.start_time
   end_time               = each.value.end_time

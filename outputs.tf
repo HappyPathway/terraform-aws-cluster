e7ef4e6a2ec278fa992ee354f7a31d3e1a5dec37
Dave Arnold <dave@roknsound.com>
2025-01-31 17:23:30 -0800

    Remove launch configuration output from Terraform outputs

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "ami" {
   value = data.aws_ami.ami
 }
 
-output "launch_configuration" {
-  value = aws_launch_configuration.lc
-}
-
 output "cloud_init" {
   value = data.cloudinit_config.cloud_init
 }

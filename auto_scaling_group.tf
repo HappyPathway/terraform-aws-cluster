197931ed5473fd1f8577b78984ff625a87ddf52e
Dave Arnold <dave@roknsound.com>
2025-01-31 16:36:51 -0800

    Fix launch template specification in autoscaling group to use correct attributes

--- auto_scaling_group.tf
+++ auto_scaling_group.tf
@@ -136,8 +136,8 @@ resource "aws_autoscaling_group" "asg_mixed_instances_policy" {
       for_each = var.auto_scaling.mixed_instances_policy.launch_template == null ? [] : [1]
       content {
         launch_template_specification {
-          launch_template_id = var.auto_scaling.mixed_instances_policy.launch_template.id
-          version            = var.auto_scaling.mixed_instances_policy.launch_template.latest_version
+          launch_template_id = var.auto_scaling.mixed_instances_policy.launch_template.launch_template_id
+          version            = var.auto_scaling.mixed_instances_policy.launch_template.version
         }
       }
     }

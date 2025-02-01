c6e06809f6d16771650a6892b7ad58283db77044
Dave Arnold <dave@roknsound.com>
2025-01-31 20:20:48 -0800

    Remove unused output variables from autoscaling group configuration

--- outputs.tf
+++ outputs.tf
@@ -9,15 +9,3 @@ output "cloud_init" {
 output "autoscaling_group" {
   value = local.autoscaling_group
 }
-
-output "launch_template" {
-  value = try(aws_launch_template.test, null)
-}
-
-output "subnet" {
-  value = try(aws_subnet.test, null)
-}
-
-output "placement_group" {
-  value = try(aws_placement_group.test, null)
-}
